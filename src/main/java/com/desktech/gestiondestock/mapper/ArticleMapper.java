d913a1380f7b08fdb42070903f96548a8d732386
kevindeffo <83418696+kevindeffo@users.noreply.github.com>
2023-09-07 21:56:32 +0100

    nothing special

--- src/main/java/com/desktech/gestiondestock/mapper/ArticleMapper.java
+++ src/main/java/com/desktech/gestiondestock/mapper/ArticleMapper.java
@@ -3,12 +3,13 @@ package com.desktech.gestiondestock.mapper;
 import com.desktech.gestiondestock.dto.ArticleDto;
 import com.desktech.gestiondestock.model.Article;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, nullValueCheckStrategy =  NullValueCheckStrategy.ALWAYS)
 public interface ArticleMapper {
-    ArticleMapper INSTANCE = Mappers.getMapper(ArticleMapper.class);
     ArticleDto articleToDto(Article article);
 
-    Article DtoToArticle();
+    Article DtoToArticle(ArticleDto articleDto);
 }
